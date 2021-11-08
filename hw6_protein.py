657b3dd9f331fdb4bc3b5f4a66fd8144f8b5c634
Sunitha101199 <sunitha101199@msitprogram.net>
2021-11-08 09:42:41 +0530

    updated Find Common Proteins

--- hw6_protein.py
+++ hw6_protein.py
@@ -123,7 +123,11 @@ Parameters: 2D list of strs ; 2D list of strs
 Returns: 2D list of strs
 '''
 def commonProteins(proteinList1, proteinList2):
-    return
+    common_Proteins = []
+    for i in proteinList1:
+        if i in proteinList2 and i not in common_Proteins:
+                common_Proteins.append(i)
+    return common_Proteins
 
 
 '''
@@ -238,12 +242,12 @@ if __name__ == "__main__":
     runWeek1()
 
     ## Uncomment these for Week 2 ##
-    """
+    
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     runWeek2()
-    """
+    
 
     ## Uncomment these for Week 3 ##
     """
